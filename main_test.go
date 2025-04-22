e52819e4cfc01f67dda0c3b5bcb3f073e3c30f4d
Chris Caldwell <chriscaldwell2026@u.northwestern.edu>
2025-04-21 21:22:19 -0700

    added TestParseRecords

--- main_test.go
+++ main_test.go
@@ -17,11 +17,16 @@ const (
 )
 
 // Intermediate step "constants"
-var goldenHeaders = [5]string{"name", "age", "DOB", "tacos", "burritos"}
-var goldenRecords = [3][5]string{
+var goldenHeaders = []string{"name", "age", "DOB", "tacos", "burritos"}
+var goldenRecords = [][]string{
 	{"Chris", "47", "04-09-1978", "35", "24"},
 	{"Carl", "23", "12-12-2002", "3", "1"},
 	{"Carla", "7", "03-05-2018", "6", "3"},
+}
+var goldenMap = [3]map[string]interface{}{
+	{"name": "Chris", "age": "47", "DOB": "04-09-1978", "tacos": "35", "burritos": "24"},
+	{"name": "Carl", "age": "23", "DOB": "12-12-2002", "tacos": "3", "burritos": "1"},
+	{"name": "Carla", "age": "7", "DOB": "03-05-2018", "tacos": "6", "burritos": "3"},
 }
 
 func TestReadCsv(t *testing.T) {
@@ -29,12 +34,35 @@ func TestReadCsv(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	testRecords, testHeaders, err := readCsv(inputFile)
-	if !reflect.DeepEqual(goldenHeaders[:], testHeaders) {
-		t.Logf("expected headers:\n%v", csvHeaders)
+	if !reflect.DeepEqual(goldenHeaders, testHeaders) {
+		t.Logf("expected headers:\n%v", goldenHeaders)
 		t.Logf("result headers:\n%v", testHeaders)
 		t.Error("Result headers do not match expected")
 	}
+	if !reflect.DeepEqual(goldenRecords, testRecords) {
+		t.Logf("expected records:\n%v", goldenRecords)
+		t.Logf("result records:\n%v", testRecords)
+		t.Error("Result records do not match expected")
+	}
+	if err != nil {
+		t.Error("Error extracting csv data")
+	}
+
+}
+
+func TestParseRecords(t *testing.T) {
+	testMap, err := parseRecords(goldenRecords, goldenHeaders)
+	if !reflect.DeepEqual(goldenMap, testMap) {
+		t.Logf("expected map:\n%v", goldenMap)
+		t.Logf("result map:\n%v", testMap)
+		t.Error("Result map does not match expected")
+	}
+	if err != nil {
+		t.Error("Error compiling data into map structure")
+	}
+}
+
+func TestBuild(t *testing.T) {
 
 }
